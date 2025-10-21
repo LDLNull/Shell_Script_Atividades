f117ef991ceab4519ad35e22c15bb2b0de8da6f4
Lucas <lucas.lima17@estudante.ifto.edu.br>
2025-10-21 20:48:05 -0300

    Second commit

---
+++ exercicio1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#script: escreve a mensagem "Olá, mundo!" no terminal
+
+#define a variavel
+mensagem="Olá, mundo!"
+
+echo $mensagem 
+
---
+++ exercicio2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#script: some de dois numeros
+
+numero1=6 
+numero2=20
+
+resultado=$((numero1 + numero2))
+
+echo "A soma de $numero1 e $numero2 é $resultado."
